4cbf7ae598a094ff0c322a6e46816080d5a5870f
Alex Phillip <alexjoelphillip@gmail.com>
2022-10-12 20:06:23 +0100

    styling

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root"); // ORIGINAL GIVEN CODE
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`; ORIGINAL GIVEN CODE
+  let howManyEpisodes = document.createElement("p");
+  howManyEpisodes.innerText = `Got ${episodeList.length} episode(s)`
+  howManyEpisodes.style.color = "red"
+  rootElem.appendChild(howManyEpisodes)
   
   const episodeContainer = document.getElementById("episodeContainer");
   
   episodeList.forEach(episode => {
     let thisEpisode = document.createElement("article");
-    thisEpisode.style.listStyle = "none";
-    thisEpisode.style.backgroundColor = "blue";
     thisEpisode.setAttribute("id", "episode");
 
     let heading = document.createElement("h1");
+    heading.setAttribute("id", "heading")
     episode["season"] < 10
       ? (heading.innerText = `${episode["name"]} - S0${episode["season"]}E${episode["number"]}`)
       : (heading.innerText = `${episode["name"]} - S${episode["season"]}E${episode["number"]}`);
@@ -26,10 +28,12 @@ function makePageForEpisodes(episodeList) {
     thisEpisode.appendChild(heading);
     
     let thumb = document.createElement("img");
+    thumb.setAttribute("id", "thumb");
     thumb.src = episode["image"]["medium"];
     thisEpisode.appendChild(thumb);
 
     let summary = document.createElement("p");
+    summary.setAttribute("id", "thumb");
     summary.innerText = episode["summary"].replace("<p>", "").replace("</p>", "");
     thisEpisode.appendChild(summary);
 

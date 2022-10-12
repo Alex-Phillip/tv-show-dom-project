ae5c5f1ca3058bb26c4c15798336102b819ac06f
Alex Phillip <alexjoelphillip@gmail.com>
2022-10-12 18:14:31 +0100

    appended heading, image and summary for each episode

--- script.js
+++ script.js
@@ -5,8 +5,37 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const rootElem = document.getElementById("root"); // ORIGINAL GIVEN CODE
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`; ORIGINAL GIVEN CODE
+  
+  const episodeContainer = document.getElementById("episodeContainer");
+
+  const episode = document.createElement("ul");
+
+  episodeList.forEach(episode => {
+    let thisEpisode = document.createElement("li");
+    thisEpisode.style.listStyle = "none";
+
+    let heading = document.createElement("h1");
+    episode["season"] < 10
+      ? (heading.innerText = `${episode["name"]} - S0${episode["season"]}E${episode["number"]}`)
+      : (heading.innerText = `${episode["name"]} - S${episode["season"]}E${episode["number"]}`);
+    episode["number"] < 10
+      ? (heading.innerText = `${episode["name"]} - S0${episode["season"]}E0${episode["number"]}`)
+      : (heading.innerText = `${episode["name"]} - S0${episode["season"]}E${episode["number"]}`);
+    thisEpisode.appendChild(heading);
+    
+    let thumb = document.createElement("img");
+    thumb.src = episode["image"]["medium"];
+    thisEpisode.appendChild(thumb);
+
+    let summary = document.createElement("p");
+    summary.innerText = episode["summary"].replace("<p>", "").replace("</p>", "");
+    thisEpisode.appendChild(summary);
+
+    episodeContainer.appendChild(thisEpisode)
+  })
 }
 
-window.onload = setup; 
+window.onload = setup;
+

e4afecc2f0ebb1b3f7730a2e02282a03da6d1d68
Alex Phillip <alexjoelphillip@gmail.com>
2022-10-18 01:18:04 +0100

    tidy up JS

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-//You can edit ALL of the code here
-
+// Create variable containing episodes object
 const allEpisodes = getAllEpisodes();
 
-const header = document.getElementById("header");
-let howManyEpisodesDisplayed = document.createElement("p");
-const main = document.getElementById("main");
-const footer = document.getElementById("footer");
-let episodesContainer = document.getElementById("episodesContainer");
+// Create function to call makePagesForEpisodes(#.) on allEpisodes(#.) on first page load(#.)   
+function setup() {
+  makePageForEpisodes(allEpisodes);
+}
 
-// Create search box and live search functionality
+// Create variable link to header
+const header = document.getElementById("header");
+// Create variable link to main <div>
+const main = document.getElementById("main");
+// Create variable link to container <div>
+let episodesContainer = document.getElementById("episodesContainer");
+// Create variable link to footer
+const footer = document.getElementById("footer");
+
+// Create input box and add live search functionality,
 let searchFunctionality = () => {
   const searchBox = document.createElement("input");
   searchBox.setAttribute("id", "searchBox");
   searchBox.setAttribute("type", "search");
   searchBox.setAttribute("placeholder", "Search...");
   header.appendChild(searchBox);
+
   searchBox.addEventListener("input", (event) => {
     searchTerm = event.target.value.toLowerCase();
     let results = allEpisodes.filter((episode) => {
@@ -23,101 +31,112 @@ let searchFunctionality = () => {
         episode["summary"].toLowerCase().includes(searchTerm)
       );
     });
+
     let clearPage = document.getElementById("episodesContainer");
     clearPage.innerHTML = "";
+    
     makePageForEpisodes(results);
   });
 };
 
-// Create dropdown and dropdown functionality
+// Create dropdown menu single episode selection functionality
 for (let key of allEpisodes) {
   let option = document.createElement("option");
   option.setAttribute("value", key.name);
 
-  option.innerText = `S${key.season.toString().padStart(2, 0)}E${key.number.toString().padStart(2, 0)} - ${key["name"]}`;
+  option.innerText = `S${key.season.toString().padStart(2, 0)}E${key.number
+    .toString()
+    .padStart(2, 0)} - ${key["name"]}`;
 
   episodesDropdown.appendChild(option);
-};
-
- episodesDropdown.addEventListener("change", (episode) => {
-    let currentEpisode = allEpisodes.filter((e) => {
-      return (
-        episode.target.value === e.name
-      )
-    })
-
-    let clear = document.getElementById("episodesContainer");
-    clear.innerHTML = "";
-    episode.target.value === 'show-all-episodes' ? makePageForEpisodes(allEpisodes) : 
-    makePageForEpisodes(currentEpisode)});
-
-  
-  
-
-// Create p element to give credit to TVMaze.com and append to footer
-let credit = document.createElement("p");
-credit.innerText = "All data sourced from ";
-footer.appendChild(credit);
-// Create link element to link to TVMaze.com (open in new tab) and append to <p>
-let tvmaze = document.createElement("a");
-tvmaze.setAttribute("target", "_blank");
-tvmaze.innerText = "TVMaze.com";
-tvmaze.href = "https://www.tvmaze.com/";
-credit.appendChild(tvmaze);
-
-function setup() {
-  makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  // Create p element to show how many episodes are currently displayed and append to header
-  howManyEpisodesDisplayed.setAttribute("id", "howManyEpisodesDisplayed");
-  howManyEpisodesDisplayed.innerText = `Displaying ${episodeList.length} of 73 episode(s)`;
+episodesDropdown.addEventListener("change", (episode) => {
+  let currentEpisode = allEpisodes.filter((e) => {
+    return episode.target.value === e.name;
+  });
+
+  let clear = document.getElementById("episodesContainer");
+  clear.innerHTML = "";
+  episode.target.value === "show-all-episodes"
+    ? makePageForEpisodes(allEpisodes)
+    : makePageForEpisodes(currentEpisode);
+});
+
+// Create <p> element to dynamically display how many of the total episodes are shown(#.),
+let howManyEpisodesDisplayed = document.createElement("p");
+// and set id
+howManyEpisodesDisplayed.setAttribute("id", "howManyEpisodesDisplayed");
+
+// Create <p> element to give credit to TVMaze.com,
+let credit = document.createElement("p");
+credit.innerText = "All data sourced from ";
+// create <a> element to link text to TVMaze.com (open in new tab),
+let tvmaze = document.createElement("a");
+tvmaze.innerText = "TVMaze.com";
+tvmaze.href = "https://www.tvmaze.com/";
+tvmaze.setAttribute("target", "_blank");
+// and append to <p>,
+credit.appendChild(tvmaze);
+// and append <p> to footer
+footer.appendChild(credit);
+
+// Create function to display episode(s):
+function makePageForEpisodes(allEpisodes) {
+  // Set text content to display how many of the total episodes are on shown(#.),
+  howManyEpisodesDisplayed.innerText = `Displaying ${allEpisodes.length} of 73 episode(s)`;
+  // and append to header
   header.appendChild(howManyEpisodesDisplayed);
 
-  // ForEach loop iterates through array of episodes and for each object in the array:
-  episodeList.forEach((episode) => {
-    // 1. Creates an article element
+  // Iterate through the episodes object, and for each episode,
+  allEpisodes.forEach((episode) => {
+
+    // Create an <article> element to display episode information and image,
     let thisEpisode = document.createElement("article");
+    // and set id
     thisEpisode.setAttribute("id", "episode");
 
-    // 2a. Creates a h1 element
+    // Create a <h1> element to display episode title,
     let episodeTitle = document.createElement("h1");
+    // and set id,
     episodeTitle.setAttribute("id", "episodeTitle");
-    // 2b. If statement to display episode name, season and number in <h1> with numbers padded to min. 2 digits
+    // and set text to display episode name, season and number (numbers padded with "0" to min. 2 digits)
     episode["season"] < 10
       ? (episodeTitle.innerText = `${episode["name"]} - S0${episode["season"]}E${episode["number"]}`)
       : (episodeTitle.innerText = `${episode["name"]} - S${episode["season"]}E${episode["number"]}`);
     episode["number"] < 10
       ? (episodeTitle.innerText = `${episode["name"]} - S0${episode["season"]}E0${episode["number"]}`)
       : (episodeTitle.innerText = `${episode["name"]} - S0${episode["season"]}E${episode["number"]}`);
-    // 2c. Appends <h1> to article
+    // and append to <article>
     thisEpisode.appendChild(episodeTitle);
 
-    // 3a. Creates an img element to show episode thumb
+    // Create an <img> element to display episode thumbnail,
     let thumb = document.createElement("img");
+    // and set id,
     thumb.setAttribute("id", "thumb");
-    // 3b. Sets <img> src to episode medium image
+    // and set src to medium image,
     thumb.src = episode["image"]["medium"];
-    // 3c. Appends <img> to article
+    // and append to <article>
     thisEpisode.appendChild(thumb);
 
-    // 4a. Creates a p element to show episode summary
+    // Create a <p> element to display episode summary,
     let episodeSummary = document.createElement("p");
+    // and set id,
     episodeSummary.setAttribute("id", "episodeSummary");
-    // 4b. Sets <p> text to summary paragraph
+    // and set text to summary paragraph (cutting out "<p>" tags),
     episodeSummary.innerText = episode["summary"]
       .replace("<p>", "")
       .replace("</p>", "");
-    // 4c. Appends <p> to article
+    // and append to <article>
     thisEpisode.appendChild(episodeSummary);
 
-    // 5. Appends article to container div
+    // Append <article> to container <div>(#.)
     episodesContainer.appendChild(thisEpisode);
   });
-}
+};
 
-// Calls live search function
+// Call live search function(#.)
 searchFunctionality();
 
+// Call setup function(#) on first page load 
 window.onload = setup;
